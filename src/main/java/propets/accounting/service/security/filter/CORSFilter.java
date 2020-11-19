71353da711ad43781740101d9ff7632641526c03
den_z <den_z@DESKTOP-4HE61H0>
2020-11-19 12:07:37 +0200

    CORS

--- src/main/java/propets/accounting/service/security/filter/CORSFilter.java
+++ src/main/java/propets/accounting/service/security/filter/CORSFilter.java
@@ -25,11 +25,11 @@ public class CORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest) req;
-		response.setHeader("Access-Control-Allow-Origin", originUrl);
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "X-Token, content-type, Authorization");
-		if ("OPTIONS".equals(request.getMethod())) {
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else { 
             chain.doFilter(request, response);
